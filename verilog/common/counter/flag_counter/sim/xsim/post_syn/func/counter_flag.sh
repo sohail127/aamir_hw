c768644090836180d03495b633a8c2b18ee8cf80
aamir <sohailnagra135@outlook.com>
2022-04-21 18:06:19 +0200

    :

---
+++ verilog/common/counter/flag_counter/sim/xsim/post_syn/func/counter_flag.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+###########################################################
+# Creat simulation snapshot using xelab
+# xelab -prj <project_file> \ 
+# -s <simulation snapshot> <library>.<top_unit>
+###########################################################
+
+# xelab --debug typical 					\
+# --maxdelay 						 					\
+# --mt 8 													\
+# -L xil_defaultlib 							\
+# -L secureip  \
+# -L simprims_ver  								\
+# -prj counter_flag.prj 					\
+# -s counter_flag 								\
+# -transport_int_delays 					\
+# -pulse_r 0 											\
+# -pulse_int_r 0 									\
+# -pulse_e 0 		 									\
+# -pulse_int_e 0 									\
+# xil_defaultlib.flag_counter_tb xil_defaultlib.glbl
+
+# xelab -wto cbe395c45f614551adef5ce6598a3ab7 \
+
+xelab -prj counter_flag.prj 					\
+--incr --debug typical 								\
+--relax --mt 8 				 								\
+--maxdelay 		 				 								\
+-L xil_defaultlib 		 								\
+-L unimacro_ver 											\
+-L xilinxcorelib_ver  								\
+-L unisims_ver  											\
+-L simprims_ver 			 								\
+-L secureip 					 								\
+--snapshot counter_flag							  \
+-transport_int_delays 								\
+-pulse_r 0 														\
+-pulse_int_r 0												\
+-pulse_e 0 														\
+-pulse_int_e 0 												\
+xil_defaultlib.flag_counter_tb xil_defaultlib.glbl -log elaborate.log
---
+++ verilog/common/counter/flag_counter/sim/xsim/post_syn/func/xsim_sim.sh
@@ -0,0 +1,5 @@
+xsim counter_flag -gui  	\
+-wdb simulate_xsim.wdb 		\
+-view simulate_xsim.wcfg  
+
+
