98fb6a415b3b9119726310eefe9542a2db65c159
aamir <sohailnagra135@outlook.com>
2022-05-05 00:22:20 +0200

    env and config updated

--- uvm/common/fifo/uvm_tb/config/fifo_config.sv
+++ uvm/common/fifo/uvm_tb/config/fifo_config.sv
@@ -1,9 +1,11 @@
 class fifo_config extends  uvm_object;
 	// component utils
 	`uvm_object_utils(fifo_config)
-	// class constructor function
-	function void new ();
-		
-	endfunction : new	
 	
+	// class constructor function
+  function new (string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction : new
+ 
+
 endclass : fifo_config
--- uvm/common/fifo/uvm_tb/env/fifo_env.sv
+++ uvm/common/fifo/uvm_tb/env/fifo_env.sv
@@ -1,3 +1,31 @@
 class fifo_env extends  uvm_env;
+	// component utils
+	`uvm_component_utils(fifo_env)
 	
+	//Data members here
+	//configs
+	fifo_config fifo_cnfg;
+	// agents
+	in_agent 	in_ag ;
+	out_agent out_ag;
+	//scorbaord
+	fifo_scoreboard fifo_scb;
+
+	// class constructor function
+  function new (string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction : new
+
+  //build phase
+	function void build_phase(uvm_phase phase);
+    `uvm_info(get_name(),$sformatf("___________BUILD_PHASE____________"), UVM_LOW)
+    super.build_phase(phase);
+    // build data members here
+    fifo_cnfg = fifo_config		 ::type_id::create("fifo_cnfg",this);
+    in_ag 		= in_agent 	 		 ::type_id::create("in_ag",		 this);
+    out_ag 		= out_agent	 		 ::type_id::create("out_ag",	 this);
+    fifo_scb 	= fifo_scoreboard::type_id::create("fifo_scb", this);
+	endfunction : build_phase
+	
+ 
 endclass : fifo_env
